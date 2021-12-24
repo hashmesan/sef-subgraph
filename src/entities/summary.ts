01b400f3089d757cdcb8b65045fa333787bad245
Junta <junta8918@gmail.com>
2021-12-24 13:31:22 +0900

    add prettier, clean up codes

--- src/entities/summary.ts
+++ src/entities/summary.ts
@@ -1,24 +1,21 @@
-import {Summary} from "../../generated/schema"
-import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts'
+import { Summary } from "../../generated/schema";
+import { Address, BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 
-export const BIG_DECIMAL_ZERO = BigDecimal.fromString('0')
-export const BIG_INT_ZERO = BigInt.fromI32(0)
+export const BIG_DECIMAL_ZERO = BigDecimal.fromString("0");
+export const BIG_INT_ZERO = BigInt.fromI32(0);
 
 export function getSummary(): Summary {
-    let bar = Summary.load("1")
-  
-    if (bar === null) {
-      bar = new Summary('1');
-      bar.walletCount = BIG_INT_ZERO
-      bar.txCount = BIG_INT_ZERO
-      bar.feePaid = BIG_DECIMAL_ZERO
-      bar.feeCollected = BIG_DECIMAL_ZERO
-      bar.currentBlock = BIG_INT_ZERO
-      bar.totalDeposits = BIG_DECIMAL_ZERO
-      bar.totalWithdrawals = BIG_DECIMAL_ZERO
-      bar.save()
-    }
-  
-    return bar as Summary
+  let bar = Summary.load("1");
+
+  if (bar === null) {
+    bar = new Summary("1");
+    bar.walletCount = BIG_INT_ZERO;
+    bar.txCount = BIG_INT_ZERO;
+    bar.currentBlock = BIG_INT_ZERO;
+    bar.totalDeposits = BIG_DECIMAL_ZERO;
+    bar.totalWithdrawals = BIG_DECIMAL_ZERO;
+    bar.save();
   }
-  
+
+  return bar as Summary;
+}
